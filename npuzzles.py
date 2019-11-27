5a212f79a3b7fce7afc545989b8764b848506f01
Tim de Jong <tide-jon@codam.nl>
2019-11-28 00:52:26 +0100

    implemented state class and closedset dictionary

--- npuzzles.py
+++ npuzzles.py
@@ -14,9 +14,8 @@ import heapq
 
 class	Puzzle():
 
-	def __init__(self, size, state):
+	def __init__(self, size):
 		self.size = size
-		self.state = state
 
 	def get_goal(self):
 		def rotate(rows, cols, x):
@@ -30,30 +29,45 @@ class	Puzzle():
 			for x, _ in enumerate(goal_array[y]):
 				self.goal[goal_array[y][x]] = (y, x)
 
-# 	OpenSet
-# 	ClosedSet
 
+class	State():
 
-def	manhattan_distance(puzzle):
+	def __init__(self, matrix, puzzle):
+		self.state = matrix
+		self.parent = 0
+		self.h = manhattan_distance(matrix, puzzle)
+
+def	manhattan_distance(state, puzzle):
 	h = 0
-	for y, _ in enumerate(puzzle.state):
-		for x, _ in enumerate(puzzle.state[y]):
-			y2, x2 = puzzle.goal[puzzle.state[y][x]]
+	for y, _ in enumerate(state):
+		for x, _ in enumerate(state[y]):
+			y2, x2 = puzzle.goal[state[y][x]]
 			h += abs(x - x2) + abs(y - y2)
 	return h
 
-size = 3
 
-state = [[2, 1, 3], [4, 5, 6], [7, 8, 0]]
-puzzle = Puzzle(size, state)
+size = 3
+puzzle = Puzzle(size)
 puzzle.get_goal()
 
-print (manhattan_distance(puzzle))
+start_state = State([[2, 1, 3], [4, 5, 6], [7, 8, 0]], puzzle)
+another_state = State([[1, 2, 3], [4, 5, 6], [7, 8, 0]], puzzle)
+
+print (start_state.h)
 
 # heap tests
-heap = []
-heapq.heappush(heap, (manhattan_distance(puzzle), puzzle.state))
-heapq.heappush(heap, (12, [[1, 2, 3], [4, 5, 6], [7, 8, 0]]))
+openset = []
+heapq.heappush(openset, (start_state.h, start_state))
+heapq.heappush(openset, (another_state.h, another_state))
 
-print (heapq.heappop(heap))
-print (heapq.heappop(heap))
+print (heapq.heappop(openset))
+print (heapq.heappop(openset))
+
+closedset = {}
+
+#	check to see if a state has been seen before. if not; adds state to closedset and openset
+
+# thing = tuple(tuple(item) for item in start_state)
+if not closedset.get(start_state):
+	closedset[start_state] = 1
+	heapq.heappush(openset, start_state.h)
