55a890beb8c28ba20afd37973f529052052330e2
Tim De jong <tide-jon@f1r2s19.codam.nl>
2019-12-28 22:37:33 +0100

    working on the can_be_solved function

--- npuzzles.py
+++ npuzzles.py
@@ -6,7 +6,7 @@
 #    By: tide-jon <tide-jon@student.codam.nl>         +#+                      #
 #                                                    +#+                       #
 #    Created: 2019/11/25 13:33:51 by tide-jon       #+#    #+#                 #
-#    Updated: 2019/12/04 17:52:29 by tide-jon      ########   odam.nl          #
+#    Updated: 2019/12/28 22:31:55 by tide-jon      ########   odam.nl          #
 #                                                                              #
 # **************************************************************************** #
 
@@ -23,7 +23,7 @@ TIME = 0
 SPACE = 0
 MOVES = 0
 
-class	Puzzle():
+class	Puzzle:
 
 	def __init__(self, size):
 		self.size = size
@@ -34,11 +34,11 @@ class	Puzzle():
 			[list(reversed(x)) for x in zip(*rotate(cols, rows - 1, x + cols))]
 			if 0 < cols \
 			else [[0]])
-		goal_array = rotate(self.size, self.size, 1)
+		self.goal_array = rotate(self.size, self.size, 1)
 		self.goal = {}
-		for y, _ in enumerate(goal_array):
-			for x, _ in enumerate(goal_array[y]):
-				self.goal[goal_array[y][x]] = (y, x)
+		for y, _ in enumerate(self.goal_array):
+			for x, _ in enumerate(self.goal_array[y]):
+				self.goal[self.goal_array[y][x]] = (y, x)
 
 class	State():
 
@@ -60,21 +60,24 @@ class	State():
 	
 	def can_be_solved(self):
 		inversions = 0
-		zero_row = 0
 		for y, _ in enumerate(self.state):
 			for x, item in enumerate(self.state[y]):
 				if item == 0:
 					zero_row = y
-				x2, y2 = x, y
+					zero_col = x
+				x2, y2 = x + 1, y
 				while y2 < puzzle.size:
 					while x2 < puzzle.size:
-						if self.state[y2][x2] < item and self.state[y2][x2] is not 0:
+						if self.state[y2][x2] \
+						and not (self.state[y2][x2] in puzzle.goal_array[(puzzle.goal[item])[0]][puzzle.goal[item][1]:]) \
+						and not any(self.state[y2][x2] in row for row in puzzle.goal_array[(puzzle.goal[item])[0]+1:][:]):
 							inversions += 1
 						x2 += 1
 					y2 += 1
 					x2 = 0
-		if (puzzle.size % 2 == 1 and inversions % 2 == 0) or \
-			(puzzle.size % 2 == 0 and inversions % 2 is not zero_row % 2):
+				print (item, inversions)
+		if (puzzle.size % 2 == 1 and (inversions % 2 is (abs(puzzle.size // 2 - zero_col) + abs(puzzle.size // 2 - zero_row)) % 2 or \
+			(puzzle.size % 2 == 0 and inversions % 2 is not abs(zero_col - (puzzle.goal[0])[1]) % 2))):
 			return True
 		return False
 		
@@ -140,10 +143,11 @@ def a_star_search(puzzle, start):
 			move.g = current.g + G
 			key = get_tuple(move.state)
 			if key not in seenset or move.g < seenset[key]:
+				if key not in seenset:
+					SPACE += 1
 				seenset[key] = move.g
 				heapq.heappush(openset, (move.g + move.h, id(move), move))
 				move.parent = current
-				SPACE += 1
 
 	print ("can't be solved")
 	return None
@@ -159,18 +163,17 @@ def	print_solution(solution, start):
 
 #	testing
 
-puzzle = Puzzle(4)
+puzzle = Puzzle(5)
 puzzle.get_goal()
 # start = State([[2, 1, 3], [4, 5, 6], [7, 0, 8]], puzzle)
-start = State([[0, 1, 2, 4], [12, 13, 3, 5], [11, 15, 14, 8], [10, 9, 7, 6]], puzzle)
-
+# start = State([[1, 2, 3, 4], [12, 14, 5, 0], [11, 13, 15, 6], [10, 9, 8, 7]], puzzle)
+start = State([[1,2,3,18,5],[16,17,22,4,6],[15,24,19,21,7],[14,11,0,20,8],[13,23,12,10,9]], puzzle)
 
 #	we first check if the starting state is solveable
 
 if not start.can_be_solved():
 	print ("can't be solved")
 	quit()
-
 
 #	execute the algorithm
 
